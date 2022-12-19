07a6157fb018b11a23b5c42bd1cb006021e69843
ColeDuval <121055244+ColeDuval@users.noreply.github.com>
2022-12-19 15:57:38 -0500

    Update Home-Page.py

--- Final-Project/Home-Page.py
+++ Final-Project/Home-Page.py
@@ -29,6 +29,27 @@ background-color: #FFB2B2;
 }
 </style>
 '''
+
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
 st.markdown(pagestyle, unsafe_allow_html=True)  # Sets The overall styling for the website by injecting CSS
 st.title('Welcome to My Website!')  # Prints a title
 st.subheader('What is the purpose of this website?')  # Prints a header
