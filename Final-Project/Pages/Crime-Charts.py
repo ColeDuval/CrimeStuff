7352433cd0c6c9d7a46bd71efb9f217dee3e95a3
ColeDuval <121055244+ColeDuval@users.noreply.github.com>
2022-12-19 15:56:47 -0500

    Update Crime-Charts.py

--- Final-Project/Pages/Crime-Charts.py
+++ Final-Project/Pages/Crime-Charts.py
@@ -26,29 +26,6 @@ pagestyle = '''
     }
     </style>
     '''
-# Contents of ~/my_app/streamlit_app.py
-import streamlit as st
-
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
-
-def page2():
-    st.markdown("# Page 2 ❄️")
-    st.sidebar.markdown("# Page 2 ❄️")
-
-def page3():
-    st.markdown("# Page 3 🎉")
-    st.sidebar.markdown("# Page 3 🎉")
-
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
-}
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
 st.markdown(pagestyle, unsafe_allow_html=True)
 list = []
 file = open(r'Final-Project/Pages/BostonCrime2021_7000_sample.csv')
