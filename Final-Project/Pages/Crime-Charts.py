56fc09e97ce2362ff0d87543ec96e3fce253c9a9
ColeDuval <121055244+ColeDuval@users.noreply.github.com>
2022-12-19 15:11:21 -0500

    Update Crime-Charts.py

--- Final-Project/Pages/Crime-Charts.py
+++ Final-Project/Pages/Crime-Charts.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import csv as csv
 import numpy as np
- st.set_page_config(
+st.set_page_config(
         page_title="Crime Charts",
         page_icon="Final-Project/Icons/blobfish.png",
     )
