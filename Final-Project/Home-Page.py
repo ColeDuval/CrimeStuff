d8d15a20ff3b71125b27081ce1003aa667db7fd1
ColeDuval <121055244+ColeDuval@users.noreply.github.com>
2022-12-19 19:48:23 -0500

    Update Home-Page.py

--- Final-Project/Home-Page.py
+++ Final-Project/Home-Page.py
@@ -30,9 +30,9 @@ background-color: #FFB2B2;
 </style>
 '''
 st.markdown(pagestyle, unsafe_allow_html=True)  # Sets The overall styling for the website by injecting CSS
-st.title('Welcome to My Website!')  # Prints a title
+st.title('Welcome to the website!')  # Prints a title
 st.subheader('What is the purpose of this website?')  # Prints a header
-st.write('the purpose of this website is to use and analyze Boston police crime data and display it to the end user of this website in a interactive fashion, to this end add this website consists of three different pages each with its own unique function and  way to interact with the data.')
+st.write('the purpose of this website is to use Boston police crime data and display it to the end user of this website in a interactive fashion, to this end add this website consists of three different pages each with its own unique function and way to interact with the data.')
 st.subheader('A Little Bit About Me') # Prints a header
 st.write('My name is Cole Duval Im currently 21 years old and I currently go to Bentley  university studying to get my undergraduate in computer information systems. I am on the cross country team at Bentley and Im also in several clubs I love getting outside and being active and my favorite activities include running coding and playing the guitar. ')
 st.image('Final-Project/Icons/IMG_9903.jpg', caption='a picture of me',width=200) # shows an image of me
